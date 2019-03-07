ebd46aa52c156d0815a682f10ccb3320e417ade0
mhxiong <mhxiong@yuansuan.cn>
2019-03-07 14:38:32 +0800

    03/07/19

--- quick_start/cube.js
+++ quick_start/cube.js
@@ -1,3 +1,4 @@
+import './cube.css'
 import {
   Scene,
   PerspectiveCamera,
@@ -12,7 +13,7 @@ var scene = new Scene()
 var camera = new PerspectiveCamera(
   // field of view (单位为度), 表示一个人通过一个光学仪器所能观测到的区域范围
   // 在0-180degree的正常范围内，值越大，观测到的东西越多，但相应的物体会显得小
-  150,
+  75,
   window.innerWidth / window.innerHeight, // aspect ratio 屏幕纵横比
   0.1, // near clipping plane 离camera太近或太远的物体都不会被渲染
   1000 // far clipping plane
@@ -20,20 +21,21 @@ var camera = new PerspectiveCamera(
 // 渲染器 (决定以怎样的方式来绘制渲染的结果)
 var renderer = new WebGLRenderer()
 // 设置渲染器的大小为窗口的内宽度，也就是内容区的宽度
-renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setSize(window.innerWidth / 2, window.innerHeight / 2)
 // domElement元素, 表示渲染器中的画布, 所有的渲染都是画在domElement上的
 document.body.appendChild(renderer.domElement)
 
 // 创建一个正方体或长方体(前三个参数分别是长宽高)
-var geometry = new CubeGeometry(2, 2, 1)
+var geometry = new CubeGeometry(2, 2, 2)
 // 填色块
 var material = new MeshBasicMaterial({ color: 0x0000ff })
 var cube = new Mesh(geometry, material)
 // 添加创建好的物体到场景中
 scene.add(cube)
-camera.position.z = 5
+camera.position.z = 5 // 当该值为0，时camera和cube重叠在一起，该值越大，说明相机离场景距离越远，物体也就看起来越小
 function render() {
-  // 使浏览器调用一次参数中的函数，形成循环
+  // 使浏览器调用一次参数中的函数，形成循环（60fps）
+  // 使用该函数来进行不间断渲染而不用setInterval, 是因为当用户暂时切换到其他tab时，渲染会暂停，节约资源
   requestAnimationFrame(render)
   cube.rotation.x += 0.1
   cube.rotation.y += 0.1
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,24 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-  entry: './quick_start/cube.js',
-  output: { 
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'quick_start'),
-  }
+  entry: './quick_start/cube.js', // 入口文件
+  output: {
+    filename: 'index.js', // 编译后的文件名
+    path: path.resolve(__dirname, 'quick_start'), // 编译后的文件存放路径
+  },
+  mode: 'development', // 开发环境
+  devtool: 'inline-source-map', // source map 用来定位错误在源文件中的位置
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'demo_01',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 }

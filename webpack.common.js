caf98094dd94553e695a739be9d169a198f162d6
mhxiong <mhxiong@yuansuan.cn>
2019-03-12 11:47:29 +0800

    分离配置文件

--- webpack.common.js
+++ webpack.common.js
@@ -6,11 +6,6 @@ module.exports = {
     filename: 'index.js', // 编译后的文件名
     path: path.resolve(__dirname, 'quick_start'), // 编译后的文件存放路径
   },
-  mode: 'development', // 开发环境
-  devtool: 'inline-source-map', // source map 用来定位错误在源文件中的位置
-  devServer: { // 用来配置webpack-dev-server
-    contentBase: './quick_start', // 指定了服务器资源的根目录，默认值是该项目的根目录(配置文件所在的那一层)
-  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'demo_01',

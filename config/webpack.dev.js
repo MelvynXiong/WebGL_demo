6a8a55e22b4f57b40e3f126a30252694c559c09e
mhxiong <mhxiong@yuansuan.cn>
2019-04-02 20:05:33 +0800

    texture纹理

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,7 +3,7 @@ const common = require('./webpack.common')
 const webpack = require('webpack')
 const path = require('path')
 module.exports = merge(common, {
-  entry: ['webpack-hot-middleware/client?reload=true', path.resolve(__dirname, '../light/index.ts')],
+  entry: ['webpack-hot-middleware/client?reload=true', path.resolve(__dirname, '../texture/index.ts')],
   output: {
     publicPath: '/',
   },
